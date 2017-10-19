418b03175abacafa455a5519b046a54c84965b39
Sandra <sandraultreras@gmail.com>
2017-10-18 21:24:26 -0700

    changed wireframe idea

---
+++ app.js
@@ -0,0 +1,7 @@
+//Constructor Function
+var profileHome = function (firstname, lastname, picture, projects){
+  this.firstName = firstname;
+  this.lastName = lastname;
+  this.img = picture;
+  this.projects = projects;
+}
